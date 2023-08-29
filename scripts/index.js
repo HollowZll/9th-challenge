bcb77d92c526f672da3c87b046ded863a198d5df
HollowZll <118689735+HollowZll@users.noreply.github.com>
2023-08-29 11:34:04 -0600

    add: all the project. All works :)

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,39 @@
 const hamburger = document.getElementById("btnID");
 const mobileMenu = document.getElementById("menu");
-const x = document.getElementById('btnClose');
-const fade = document.getElementById('fade')
+const x = document.getElementById("btnClose");
+const fade = document.getElementById("fade");
+const retroImage = document.getElementById("retroImgMob");
+const orangeButon = document.getElementById("orangeButton");
 
+hamburger.addEventListener("click", () => {
+  mobileMenu.style.visibility = "visible";
+  //  fade.style.opacity = '30%';
+  fade.style.backgroundColor = "rgb(128,127,138)";
+  retroImage.style.filter = "brightness(40%)";
+  orangeButon.style.color = "gray";
+  orangeButon.style.backgroundColor = "rgb(121,47,53)";
+  orangeButon.style.opacity = "0.8";
+  fade.style.height = "1820px";
+});
 
-hamburger.addEventListener("click", () => { 
-         mobileMenu.style.visibility = 'visible';
-        //  fade.style.opacity = '30%';
-         fade.style.backgroundColor = 'rgb(128,127,138)'
-         fade.style.height = '1000px'
-      });
+x.addEventListener("click", () => {
+  mobileMenu.style.visibility = "hidden";
+  fade.style.opacity = "100%";
+  fade.style.backgroundColor = "white";
+  fade.style.height = "1820px";
+  retroImage.style.filter = "contrast(100%)";
+  orangeButon.style.color = "white";
+  orangeButon.style.backgroundColor = "hsl(5, 85%, 63%)";
+  orangeButon.style.opacity = "1";
+  retroImage.style.opacity = "1";
+});
 
-x.addEventListener('click', () => {
-    mobileMenu.style.visibility = "hidden";
-    fade.style.opacity = '100%';
-    fade.style.backgroundColor = 'white'
-    fade.style.height = '1000px'
-})
+// let mediaQuerry = (desktop) => {
+//   if (desktop.matches) {
+//     document.mobileMenu.style.visibility = "visible";
+//   }
+// };
 
-
-
-
-
-
-
+// let desktop = window.matchMedia("(min-width: 385px)");
+// mediaQuerry(desktop);
+// desktop.addEventListener(mediaQuerry);
